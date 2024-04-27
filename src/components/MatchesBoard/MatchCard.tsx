a724c245e35520d218336ee66c4b4b384612a9d5
Pavel Terenin <pavelterenin@Pavels-MacBook-Pro.local>
2024-04-26 19:03:32 -0700

    Small style fix

--- src/components/MatchesBoard/MatchCard.tsx
+++ src/components/MatchesBoard/MatchCard.tsx
@@ -5,8 +5,8 @@ type PropTypes = {
 }
 export default function MatchCard({ header, pokemons }: PropTypes) {
     return <div className="">
-        <h3 className="font-bold ">{header}</h3>
-        <div>
+        <h3 className="font-bold  m-2 text-center">{header}</h3>
+        <div className="grid grid-cols-1 lg:grid-cols-2">
             {pokemons.map((pokemon) => {
                 return <PokemonSmallCard pokemon={pokemon} />
             })}
--- src/components/MatchesBoard/PokemonSmallCard.tsx
+++ src/components/MatchesBoard/PokemonSmallCard.tsx
@@ -7,7 +7,7 @@ type PropTypes = {
 
 export default function PokemonSmallCard({ pokemon }: PropTypes) {
     const imagSrc = pokemon.sprites.other.home.front_shiny;
-    return <Link href={pokemon.name} className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 pl-4 min-h-36">
+    return <Link href={pokemon.name} className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 pl-4 min-h-36">
         <img className="object-cover w-1rounded-t-lg md:h-auto w-20 md:rounded-none md:rounded-s-lg" src={imagSrc} alt={pokemon.name} />
         <div className="flex flex-col justify-between p-4 leading-normal">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{pokemon.name}</h5>
